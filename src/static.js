500ec76ecd38a81438523d1269cc164435ee9697
Fazli <fazliddin@mail>
2023-11-21 21:03:45 +0500

    first commit

---
+++ src/static.js
@@ -0,0 +1,10 @@
+export const options = {
+    method: 'GET',
+    headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1NjlhZjRlNTMzMzNlNDlkMDI3ZjMwNWY1Nzk2OWE4ZCIsInN1YiI6IjY1MDliNzJjMzk0YTg3MDBjNWJmNDI0NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.C4GiSee5vjj-vdfV70-rXp4_yUxCFsporOcez3FikAU'
+    }
+};
+
+export const imgUrl = 'https://image.tmdb.org/t/p/w500';
+export const imgUrlFull = 'https://image.tmdb.org/t/p/original'
