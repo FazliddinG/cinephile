a7e7e1a4a4f97ad9e9aa1b781e6e8f5697a831fb
Fazli <fazliddin@mail>
2023-11-24 19:52:29 +0500

    builded project

---
+++ dist/assets/Films-3573b1e3.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./ListContent-c65f43ee.js";import{o as a,c as o,a as s}from"./index-52ce4350.js";const t={class:"main"},i={__name:"Films",setup(c){return(_,m)=>(a(),o("main",t,[s(e,{type:"movie"})]))}};export{i as default};
---
+++ dist/assets/Serials-f1dac87a.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./ListContent-c65f43ee.js";import{o as a,c as t,a as s}from"./index-52ce4350.js";const o={class:"main"},i={__name:"Serials",setup(c){return(r,_)=>(a(),t("main",o,[s(e,{type:"tv"})]))}};export{i as default};
