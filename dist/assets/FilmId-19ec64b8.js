f6ece8743eaffcc4b540830e73f6d2bbf3a966a1
Fazli <fazliddin@mail>
2023-11-24 20:05:56 +0500

    changed base folder

--- dist/assets/FilmId-19ec64b8.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./Single-883e8959.js";import{o as a,c as o,a as t}from"./index-52ce4350.js";const s={class:"main"},i={__name:"FilmId",setup(c){return(_,m)=>(a(),o("main",s,[t(e,{type:"movie"})]))}};export{i as default};
